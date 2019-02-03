a6484c6e551ae6ff1af56d6ac0e0ee24d22846b3
Vanita Samba <panduvennu@gmail.com>
2019-02-02 23:56:15 -0600

    without refactoring

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ app.config['DEBUG'] = True
 def index():
     template= jinja_env.get_template('home_page.html')
     return template.render()
+def validate(fieldname, fieldval, fieldlen):
+    msg=''
+    if (not fieldval) or (fieldval.strip() == "") or (" " in fieldval) or (fieldlen < 3) or (fieldlen > 20):
+        msg = "Please enter a valid "+fieldname
+    return msg
+
+def validateemail(Email_Id, email_id_length):
+    msg=''
+    pattern = re.compile("^\w+[@]{1}\w+[.]{1}\w+$")
+    if (email_id_length>0) and ((" " in Email_Id) or ("@" not in Email_Id) or ("." not in Email_Id) or (email_id_length < 3) or (email_id_length > 20) or not pattern.match(Email_Id)):
+        msg = "Please enter a valid email_id."
+    return msg
+
 
 @app.route("/home", methods=['POST'])  #request handler for home page
 def welcome():
@@ -25,41 +38,34 @@ def welcome():
     User_Name_length=len(user_name)
     Password_length=len(Password)
     Confirm_Password_length=len(Confirm_Password)
-    username_error =''
-    password_error=''
+    username_error = validate("user name",user_name,User_Name_length)
+    password_error=validate("password",Password,Password_length)
     confirmpassword_error=''
-    emailId_error=''
+    emailId_error=validateemail(Email_Id,email_id_length)
     passwordmatch_error=''
 
-    #template=jinja_env.get_template('welcome_page.html')
-    #return template.render(name=user_name)
-    if (not user_name) or (user_name.strip() == "") or (" " in user_name):
-        username_error = "Please enter a valid user name."
-        #return redirect("/?error=" + error)
-    if (not Password) or (Password.strip() == "") or (" " in Password):
-        password_error = "Please enter a valid Password."
+    #if (not user_name) or (user_name.strip() == "") or (" " in user_name):
+        #username_error = "Please enter a valid user name."
+    #if (not Password) or (Password.strip() == "") or (" " in Password):
+       # password_error = "Please enter a valid Password."
     #if (not (Password == Confirm_Password) and (confirmpassword_error== "")):
     #if Confirm_Password.strip() == "":
        # Confirm_Password = "Please enter a valid Confirm_Password."
     if not Password == Confirm_Password or Confirm_Password.strip() == "" :
-        confirmpassword_error = "passwords do not match"
-    if (email_id_length>0) and ((" " in Email_Id) or ("@" not in Email_Id) or ("." not in Email_Id) or (email_id_length < 3) or (email_id_length >= 20)):
-        #pattern = re.compile("[^@]+@[^@]+\.[^@]+")
+        confirmpassword_error = "Passwords do not match"
+    # if (email_id_length>0) and ((" " in Email_Id) or ("@" not in Email_Id) or ("." not in Email_Id) or (email_id_length < 3) or (email_id_length > 20)):
+    #     emailId_error = "Please enter a valid email_id."
+    #
+    # pattern = re.compile("^\w+[@]{1}\w+[.]{1}\w+$")
+    # if (email_id_length>0) and not pattern.match(Email_Id):
+    #         emailId_error = "Please enter a valid email_id."
+    #if (User_Name_length < 3) or (User_Name_length > 20):
+     #   username_error =" Not a valid user name"
+    #if (Password_length < 3) or (Password_length > 20):
+     #   password_error =" Not a valid password"
 
-        pattern = re.compile("@.")
-        if not pattern.match(Email_Id):
-            emailId_error = "Please enter a valid email_id."
-
-    if (User_Name_length < 3) or (User_Name_length >= 20):
-        username_error =" Not a valid user name"
-
-
-
-    if (Password_length < 3) or (Password_length >= 20):
-        password_error =" Not a valid password"
     if not username_error and not password_error and not confirmpassword_error and not passwordmatch_error and not emailId_error:
         return render_template('welcome_page.html', name=user_name)
-        #return redirect('/valid-time?time={0}'.format(time))
     else:
         template=jinja_env.get_template('home_page.html')
         return template.render(username=user_name,
@@ -70,8 +76,4 @@ def welcome():
                                passwordmatch_error=passwordmatch_error,
                                emailId_error=emailId_error
                             )
-
-
-
-
 app.run()
